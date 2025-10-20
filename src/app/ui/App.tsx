547883ba9b7244f8abf3a98af3da33f7a5bf0487
molnia_grom <mgrom250@gmail.com>
2025-10-20 18:30:54 +0300

    Update project structure and routing: added react-router, reorganized App component, created new pages (Main, Profile, Playlists, Tracks), and implemented 404 Page Not Found component. Updated dependencies in package.json and pnpm-lock.yaml.

---
+++ src/app/ui/App.tsx
@@ -0,0 +1,9 @@
+import { Routing } from "@/common/routing/Routing"
+
+export const App = () => {
+  return (
+    <>
+      <Routing />
+    </>
+  )
+}
---
+++ src/common/routing/Routing.tsx
@@ -0,0 +1,26 @@
+import { MainPage } from "@/app/ui/MainPage/MainPage"
+import { ProfilePage } from "@/features/auth/ui/ProfilePage/ProfilePage"
+import { PlaylistsPage } from "@/features/playlists/ui/PlaylistsPage.tsx/PlaylistsPage"
+import { TracksPage } from "@/features/tracks/ui/TracksPage"
+import { Route, Routes } from "react-router"
+import { PageNotFound } from "../components/PageNotFound/PageNotFound"
+
+export const Path = {
+    Main: '/',
+    Playlists: '/playlists',
+    Tracks: '/tracks',
+    Profile: '/profile',
+    NotFound: '*',
+  } as const
+   
+  export const Routing = () => {
+      return (
+          <Routes>
+              <Route path={Path.Main} element={<MainPage />} />
+              <Route path={Path.Playlists} element={<PlaylistsPage />} />
+              <Route path={Path.Tracks} element={<TracksPage />} />
+              <Route path={Path.Profile} element={<ProfilePage />} />
+              <Route path={Path.NotFound} element={<PageNotFound />} />
+          </Routes>
+      )
+  }
